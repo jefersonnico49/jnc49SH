6a6e731d37461a780f1ca6bf204dd66d397b0187
Jeferson Nico Callao <Jeferson Nico Callao@LT-SHIPADMIN-G>
2020-04-04 03:59:56 +0800

    test

--- com.maritime/src/test/java/runnerMA29228_sprint02_Extrachargesvalidation/TestrunnerMA29228_Extrachargesvalidation.java
+++ com.maritime/src/test/java/runnerMA29228_sprint02_Extrachargesvalidation/TestrunnerMA29228_Extrachargesvalidation.java
@@ -11,7 +11,7 @@ import cucumber.api.junit.Cucumber;
 		plugin = {"pretty", "html:Gherkin29228_SPRINT02_addExtraChargestoperson_functionalityvalidation/cucumber-html-report","json:Gherkin29228_SPRINT02_addExtraChargestoperson_functionalityvalidation/cucumber-report.json","junit:Gherkin29228_SPRINT02_addExtraChargestoperson_functionalityvalidation/cucumber.xml"}
 		)
 
-//test//
+//test1// 
 
 public class TestrunnerMA29228_Extrachargesvalidation {
 
